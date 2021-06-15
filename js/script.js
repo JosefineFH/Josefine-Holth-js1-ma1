c6a70e10d6ec008ac1af418926dacc400ccfeadf
Josefine Holth <Josefine.Holth@hotmail.com>
2021-06-15 23:17:15 +0200

    finish assignment

--- js/script.js
+++ js/script.js
@@ -11,3 +11,67 @@ const cats = [
         age: 21
     }
 ];
+
+// * Question 1
+const cat = {
+    complain: function(){
+        console.log("!Meow");
+    }
+}
+cat.complain()
+
+// * Question 2
+const heading = document.querySelector("h3");
+heading.innerHTML = "Updated heading";
+
+// * Question 3
+heading.style.fontSize = "2em";
+
+// * Question 4
+heading.classList.add("subheading");
+
+// * Question 5
+const paragraphs = document.querySelectorAll("p");
+for(let i = 0; i < paragraphs.length; i++){
+    paragraphs[i].style.color = "red";
+}
+
+// * Question 6
+const resultsContainer = document.querySelector(".results");
+
+resultsContainer.innerHTML = "<p>New paragraph</p>";
+resultsContainer.style.backgroundColor = "yellow";
+
+// * Question 7 
+function catsList(list){
+    for(let i = 0; i < list.length; i++){
+        console.log(list[i].name);
+    }
+}
+catsList(cats);
+
+// * Question 8
+function createCats(cats){
+    let aniamlList = "";
+    
+    for(let i = 0; i < cats.length; i++){
+        if(!cats[i].age){
+            cats[i].age = "Age unknown";
+        }
+        
+        aniamlList += `
+        <div> 
+            <h5>${cats[i].name}</h5>   
+            <p>${cats[i].age}</p>
+        </div>
+        `;
+    }
+
+    const finalList = aniamlList;
+    return finalList;
+}
+
+const catList = createCats(cats);
+
+const container = document.querySelector(".cat-container");
+container.innerHTML = catList;
